4a6f2527d1376aaf5cca40f572432216416e8031
Jake Smith <jakeconlonsmith@gmail.com>
2020-08-30 14:23:28 -0500

    Update MainMenu.py

--- GradingSystem/MainMenu.py
+++ GradingSystem/MainMenu.py
@@ -7,21 +7,28 @@ fontName = 'Times New Roman'
 titleFont = Font.Font(family = fontName, size = 20)
 loginFont = Font.Font(family = fontName, size = 10)
 
+#Closes the current window
 def CloseWindow(window):
     window.destroy()
 
+#If the username and password are a match for the database, this sends them to the next screen
 def SuccessfulLogin():
     print("Successful")
 
+#If the username and/or password isn't valid, this comes up
 def FailedLogin():
     messagebox.showwarning('Incorrect Login', 'Incorrect Username and/or Password.')
 
+#Checks if the username and password are valid
 def Login(username, password):
     print('Username: ' + username + '\nPassword: ' + password)
     FailedLogin()
 
-def NewAccount(username, password):
-    FailedLogin() #placeholder
+#Window that opens when the user selects to make a new account
+def NewAccount():
+    newAccountWindow = tkinter.Tk()
+    newAccountWindow.title('New Account')
+
 
 def LoginScreen():
     #Sets the initial window, window size
@@ -46,7 +53,7 @@ def LoginScreen():
     loginButton = tkinter.Button(mainWindow, text = 'Login', command = lambda:Login(usernameBox.get(),passwordBox.get()), height = 1, width = 10)
     loginButton.place(relx = 0.4, rely = 0.6, anchor = 'center')
     #Button to create a new account
-    createAccountButton = tkinter.Button(mainWindow, text = 'New Account', command = lambda:NewAccount(usernameBox.get(), passwordBox.get()), height = 1, width = 10)
+    createAccountButton = tkinter.Button(mainWindow, text = 'New Account', command = lambda:NewAccount(), height = 1, width = 10)
     createAccountButton.place(relx = 0.5, rely = 0.7, anchor = 'center')
     #Button to close the program
     quitButton = tkinter.Button(mainWindow, text = 'Quit', command = lambda:CloseWindow(mainWindow), height = 1, width = 10)
